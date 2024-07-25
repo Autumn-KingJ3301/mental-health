04107f9c5f411431866940162eec4c8de05d7278
Jatin Kaushal <jatinkaushal3301@gmail.com>
2024-07-25 16:44:45 +0530

    First Commit- UI layout

--- app/src/main/java/com/kaushal/mentalhealth/screens/HomeScreen.kt
+++ app/src/main/java/com/kaushal/mentalhealth/screens/HomeScreen.kt
@@ -1,5 +1,6 @@
 package com.kaushal.mentalhealth.screens
 
+import android.app.Notification
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
@@ -12,7 +13,6 @@ import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
@@ -20,16 +20,11 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.Check
-import androidx.compose.material.icons.filled.List
-import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
@@ -41,7 +36,6 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
@@ -51,7 +45,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.kaushal.mentalhealth.R
 import java.time.LocalDate
 import java.time.format.TextStyle
@@ -350,7 +343,19 @@ fun Tasks() {
                 ))
             }
         }
+        // Tasks
     }
+}
+@Composable
+fun TaskCard(
+    dueDate: LocalDate,
+    title: String,
+    description: String,
+    tags: List<String>,
+    isCompleted: MutableState<Boolean>,
+    notification: MutableState<Boolean>
+){
+
 }
 
 @Preview
