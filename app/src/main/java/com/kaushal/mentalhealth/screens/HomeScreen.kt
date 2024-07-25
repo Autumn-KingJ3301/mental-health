a3040200316dee6103af263c78adef557dcb8a66
Jatin Kaushal <jatinkaushal3301@gmail.com>
2024-07-25 13:00:49 +0530

    First Commit- UI layout

--- app/src/main/java/com/kaushal/mentalhealth/screens/HomeScreen.kt
+++ app/src/main/java/com/kaushal/mentalhealth/screens/HomeScreen.kt
@@ -3,10 +3,12 @@ package com.kaushal.mentalhealth.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
@@ -14,22 +16,42 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.List
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.List
 import androidx.compose.material.icons.filled.Notifications
 import androidx.compose.material.icons.outlined.Notifications
 import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.FilterChipDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.kaushal.mentalhealth.R
+import java.time.LocalDate
+import java.time.format.TextStyle
+import java.util.Locale
 
 
 @Composable
@@ -39,9 +61,13 @@ fun HomeScreen() {
             .fillMaxWidth()
             .fillMaxHeight()
             .background(color = colorResource(id = R.color.background))
+            .padding(15.dp)
     ) {
         Spacer(modifier = Modifier.height(20.dp))
         Header()
+        Spacer(modifier = Modifier.height(10.dp))
+        Calendar()
+        TaskFilters()
     }
 }
 
@@ -49,9 +75,7 @@ fun HomeScreen() {
 @Composable
 fun Header() {
     Row(
-        modifier = Modifier
-            .padding(10.dp)
-            .fillMaxWidth(),
+        modifier = Modifier.fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
@@ -92,9 +116,7 @@ fun Header() {
                     .width(35.dp)
             ) {
                 Icon(
-                    Icons.Outlined.Search,
-                    contentDescription = "Search",
-                    tint = colorResource(
+                    Icons.Outlined.Search, contentDescription = "Search", tint = colorResource(
                         id = R.color.text
                     )
                 )
@@ -118,6 +140,145 @@ fun Header() {
                 )
             }
         }
+    }
+
+}
+
+@Composable
+fun Calendar() {
+
+    var selectedDate by remember {
+        mutableStateOf(LocalDate.now())
+    }
+    val dates = remember(selectedDate) {
+        (-2..2).map { selectedDate.plusDays(it.toLong()) }
+    }
+
+    Column(
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text(
+                text = "Calendar",
+                style = MaterialTheme.typography.titleLarge,
+                color = colorResource(
+                    id = R.color.text
+                )
+            )
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    Icons.Default.ArrowDropDown,
+                    contentDescription = "Dropdown",
+                    tint = colorResource(
+                        id = R.color.text
+                    )
+                )
+            }
+        }
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(70.dp)
+                .background(
+                    color = colorResource(id = R.color.background_secondary),
+                    shape = RoundedCornerShape(14.dp)
+                )
+                .padding(5.dp),
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            dates.forEach { date ->
+                DateItems(
+                    date = date, isSelected = date == selectedDate, taskCompleted = 0.8f
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun DateItems(
+    date: LocalDate, isSelected: Boolean, taskCompleted: Float = 1f
+) {
+    Box(
+        modifier = Modifier
+            .width(47.dp)
+            .height(72.dp)
+            .background(
+                Brush.verticalGradient(
+                    colorStops = arrayOf(
+
+                        (if (isSelected) taskCompleted else 0f) to colorResource(id = R.color.dark_accent_2),
+                        0.0f to colorResource(id = R.color.background_secondary)
+                    )
+                ), shape = RoundedCornerShape(8.dp)
+            )
+    ) {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Text(
+                text = date.dayOfMonth.toString(),
+                style = MaterialTheme.typography.titleLarge,
+                fontWeight = FontWeight.Bold,
+                color = colorResource(id = if (isSelected) R.color.text else R.color.text_accent)
+            )
+            Text(
+                text = date.dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault()),
+                style = MaterialTheme.typography.titleMedium,
+                color = colorResource(id = if (isSelected) R.color.text else R.color.text_accent)
+
+            )
+
+        }
+    }
+}
+
+@Composable
+fun TaskFilters() {
+    var todo by remember {
+        mutableStateOf(false)
+    }
+    var focusMode by remember {
+        mutableStateOf(false)
+    }
+    var periodic by remember {
+        mutableStateOf(false)
+    }
+
+    Row {
+        FilterChip(
+            onClick = { todo = !todo },
+            label = {
+                Text(
+                    "To-do", color = colorResource(id = R.color.text)
+                )
+            },
+            colors = FilterChipDefaults.filterChipColors(
+                containerColor =  Color.Transparent,
+                selectedContainerColor = colorResource(id = R.color.background_secondary)
+                ),
+            border = FilterChipDefaults.filterChipBorder(
+                borderColor = colorResource(id = R.color.dark_accent_2),
+                selected = todo,
+                enabled = true
+            ),
+            selected = todo,
+            leadingIcon = if (todo) {
+                {
+                    Icon(
+                        imageVector = Icons.Default.Check,
+                        contentDescription = "Done icon",
+                        modifier = Modifier.size(FilterChipDefaults.IconSize),
+                    )
+                }
+            } else {
+                null
+            },
+        )
     }
 
 }
